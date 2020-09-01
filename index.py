1e5c4e12a1aae0ebb29d03b34e0cf0f3914e75a8
Lam Pham <59724094+lpham2309@users.noreply.github.com>
2020-09-01 19:14:09 -0400

    commit them thong diep hello (#8)

--- index.py
+++ index.py
@@ -2,4 +2,10 @@ print('Hom nay troi qua dep')
 
 print('Ngay mai la cuoi tuan roi yea!')
 
+print('Oi troi oi nam nay het tien roi')
+
+print('Nho nha qua huhuhu')
+print('Hom nay la thu may hom nay la thu 2')
+print('Hello')
+
 print('Oi chu choa, hom nay minh co viec lam moi. Vui alm')
