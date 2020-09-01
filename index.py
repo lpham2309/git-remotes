94d64f123758b41ce2c651f174846c3e89974d5c
Tui <clbt.pham3110@gmail.com>
2020-09-01 19:00:53 -0400

    commit dau tieng gioi thieu viec lam

--- index.py
+++ index.py
@@ -2,8 +2,4 @@ print('Hom nay troi qua dep')
 
 print('Ngay mai la cuoi tuan roi yea!')
 
-print('Oi troi oi nam nay het tien roi')
-
-print('Nho nha qua huhuhu')
-print('Hom nay la thu may hom nay la thu 2')
-
+print('Oi chu choa, hom nay minh co viec lam moi. Vui alm')
