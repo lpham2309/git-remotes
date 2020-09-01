76ed5813aa5d1325f34b94826a0af3b231b7afd3
Lam Pham <59724094+lpham2309@users.noreply.github.com>
2020-09-01 18:48:48 -0400

    commit thong diep nho nha nho que huong (#6)

--- index.py
+++ index.py
@@ -2,5 +2,8 @@ print('Hom nay troi qua dep')
 
 print('Ngay mai la cuoi tuan roi yea!')
 
+print('Oi troi oi nam nay het tien roi')
+
+print('Nho nha qua huhuhu')
 print('Hom nay la thu may hom nay la thu 2')
 
